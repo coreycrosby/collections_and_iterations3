ab73f853647a1789d4c0acb575890fb12c05c32f
Corey Crosby <ccc_13@live.ca>
2019-05-15 13:43:13 -0400

    made changes using flake8

--- exercises.py
+++ exercises.py
@@ -1,27 +1,27 @@
-#Exercise 11
-one_to_onehundred = range(1,101)
+# Exercise 11
+one_to_onehundred = range(1, 101)
 
 for number in one_to_onehundred:
-    
     if number % 3 and number % 5 == 0:
         print("BitMaker")
-    
     elif number % 3 == 0:
         print("Bit")
 
     elif number % 5 == 0:
         print("Maker")
 
-    else: 
+    else:
         print(number)
 
+# Exercise 12
 
 
-#Exercise 12
 def pizzas():
     print('How many pizzas do you want to order?')
     number_pizzas = list(range(1, (int(input())+1)))
     return number_pizzas
+
+
 pizza_order = pizzas()
 for pizza in pizza_order:
     print(f'How many toppings for pizza {pizza}')
